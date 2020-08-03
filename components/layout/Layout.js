f37e5d519a18dd81794f5c85443588870c308db3
pipe473 <bedoyafelipe743@gmail.com>
2020-08-03 21:51:22 +0200

    Agregando fuentes y algunos estilos de next.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import { Global, css } from '@emotion/core';
+import Head from 'next/head';
 
 const Layout = props => {
     return ( 
@@ -43,6 +44,16 @@ const Layout = props => {
                 }
             `}
         />
+
+        <Head>
+            <html lang="es" />
+            <title>meFoodTxell</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+             integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
+             crossorigin="anonymous" />
+             <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" 
+                rel="stylesheet" />
+        </Head>
            <Header />
             <main>
                 {props.children}
