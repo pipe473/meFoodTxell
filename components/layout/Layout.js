5827b525c2acf5f1f80ef99e3a872ff1f2eec919
pipe473 <bedoyafelipe743@gmail.com>
2020-08-05 16:36:51 +0200

    Agregando las fuentes de google fonts y otros estilos de letras

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -26,11 +26,22 @@ const Layout = props => {
                 body {
                     font-size: 1.6rem;
                     line-height: 1.5;
+                    font-family: 'PT Sans', sans-serif;
                 }
 
                 h1, h2, h3 {
                     margin: 0 0 2rem 0;
                     line-height: 1.5;
+                }
+
+                h1, h2 {
+                    font-family: 'Roboto Slab', serif;
+                    font-weight: 700;
+                }
+
+                h3 {
+                    font-family: 'PT Sans', sans-serif;
+                    font-weight: 700;
                 }
 
                 ul {
@@ -51,8 +62,10 @@ const Layout = props => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
              integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
              crossorigin="anonymous" />
-             <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" 
+            <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" 
                 rel="stylesheet" />
+
+            <link href="/static/css/app.css" rel="stylesheet" />
         </Head>
            <Header />
             <main>
